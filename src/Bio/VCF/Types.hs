19cc4c3dec31c269cb1410b3a44eb3b1fd276ec4
Juan Paucar <juantotish1@hotmail.com>
2016-02-22 13:57:35 -0500

    switch to list instead of map WIP

--- src/Bio/VCF/Types.hs
+++ src/Bio/VCF/Types.hs
@@ -1,16 +1,25 @@
 module Bio.VCF.Types where
 
-import Data.HashMap (Map(..))
-
 data VCF = VCF
-          { header  :: Header
-          , records :: [Record]
+          { header     :: Header
+          , records    :: [Record]
           }
 
 data Header = Header
-              { fileFormat        :: String
-              , informationFields :: Maybe (Map String InformationField)
-              , filterFields      :: Map String FilterField
-              , alternativeAlleleField :: Map String 
+              { fileFormat              :: String
+              , informationFields       :: [InformationField]
+              , filterFields            :: [FilterField]
+              , formatFields            :: [FormatField]
+              , alternativeAlleleFields :: [AlternativeAlleleField]
+              , assemblyField           :: String
+              , contigFields            :: [ContigField]
+              , sampleFields            :: [SampleField]
+              , pedigreeFields          :: PedigreeInformation
               }
 
+data Record = Record
+              { chrom :: String --no white space
+              , pos   :: Integer
+              , id    :: [String] --no white space or semicolon
+              , ref   :: 
+              }
